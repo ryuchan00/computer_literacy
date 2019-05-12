78b7f354ab9ed8c7744146816ca5bb391b573d3f
Ryutaro Yamakawa <19238591+ryuchan00@users.noreply.github.com>
2019-05-12 10:05:43 +0900

    Update ensyu4_2a.asm

--- ensyu4_2a.asm
+++ ensyu4_2a.asm
@@ -1,13 +1,12 @@
       iload 0
 Loop: loadx Data
       ifz End
-      ifn Abs
-Cmn:  add Sum
+      ifp Skip
+      neg
+Skip: add Sum
       store Sum
       iadd 1
       jump Loop
-Abs:  neg
-      jump Cmn
 End:  stop
 Sum:  0
 Data: 1
